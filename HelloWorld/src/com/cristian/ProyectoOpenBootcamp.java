8a5970d83e35b654861e13b4ac783f3004b1fd24
cristian <cristaker1927@hotmail.com>
2022-07-14 11:51:35 -0500

    prueba bootcamp

---
+++ HelloWorld/src/com/cristian/ProyectoOpenBootcamp.java
@@ -0,0 +1,19 @@
+package com.cristian;
+
+public class ProyectoOpenBootcamp {
+
+    public static void main(String[] args) {
+
+        int a = 25;
+        long b = 265352425;
+        double c = 34.56;
+        String e = "Christian";
+        boolean d = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(d);
+    }
+}
