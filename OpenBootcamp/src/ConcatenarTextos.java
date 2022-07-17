0c08dd8f578acdee8526683e09e602b5327a555c
cristian <cristaker1927@hotmail.com>
2022-07-17 13:02:27 -0500

    elseif

---
+++ OpenBootcamp/src/ConcatenarTextos.java
@@ -0,0 +1,19 @@
+public class ConcatenarTextos {
+
+    public static void main(String[] args) {
+        nombres();
+    }
+
+    public static void nombres(){
+        String nombre1="pedro";
+        String nombre3="juan";
+
+        if (nombre3 == "christian"){
+            System.out.println("El nombre de la persona es: "+nombre3);
+        }
+        else {
+            System.out.println("El nombre es: "+nombre1);
+        }
+
+    }
+}
--- OpenBootcamp/src/FuncionesOB.java
+++ OpenBootcamp/src/FuncionesOB.java
@@ -2,10 +2,16 @@ public class FuncionesOB {
 
     public static void main(String[] args) {
         precioMasIva();
+        multiply(5.7, 6.7);
     }
     public static void precioMasIva(){
         double precio=1500;
         double precioConIva = (precio*0.57);
         System.out.println(precioConIva);
     }
-}
+
+        public static double multiply(Double a, Double b) {
+            double c=a*b;
+            return c;
+        }
+    }
